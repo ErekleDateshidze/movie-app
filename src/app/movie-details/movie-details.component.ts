601afc4274ee4b0736889ec72ac146603c490d50
Erekle99 <erekledateshidze99@gmail.com>
2023-02-17 20:23:17 +0400

    movie data

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { forkJoin, Observable, of } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap ,tap} from 'rxjs/operators';
 import { Movie ,Country } from '../api.model';
 import { MovieApiService } from '../movie-api.service';
 
@@ -16,37 +16,49 @@ export class MovieDetailsComponent implements OnInit {
   movie$: Observable<Movie>| undefined;
   releaseYear$: Observable<number>| undefined;
   actorNames$: Observable<string> | undefined;
-  currencies$: Observable<{ name: string; symbol: string; flagUrl: string; }[]> | undefined;
-
+  currencies$: Observable<any> | undefined;
+  homeData: Observable <any[]> | undefined
+ 
   constructor( private movieApiService: MovieApiService) {}
 
   ngOnInit(): void {}
 
-
   getMovieDetails() {
-    this.movieApiService.getMovieDetails(this.movieName).pipe(
+    this.homeData=this.movieApiService.getMovieDetails(this.movieName).pipe(
       switchMap((movie) => {
-        console.log(movie)
-        const actor= movie.Actors
-        const title= movie.Title;
-        const countries = movie.Country.split(', ').map((country:any) => this.fetchFlagsAndCurrencies(country));
-        return forkJoin([ of({actor}), of({title}),...countries]);
+        const actor = movie.Actors.split(',').map((a:string) => a.trim().split(' ')[0]);
+        const poster= movie.poster
+        const title = movie.Title;
+        const releaseDate = new Date(movie.Released);
+        const currentYear = new Date().getFullYear();
+        const yearsAgo = currentYear - releaseDate.getFullYear();
+        const countries = movie.Country.split(', ').map((country: any) =>
+          this.fetchFlagsAndCurrencies(country)
+        );
+        return forkJoin([of({ actor }), of({ title }), of({yearsAgo}, of({poster})), ...countries]);
+      }),tap(console.log)
+    )
+  }
+
+  private fetchFlagsAndCurrencies(country: string) {
+    return this.movieApiService.getCountryDetails(country).pipe(
+      map((x: any) => {
+        console.log(x , 'bla')
+        return x.map((element:any) => {
+          return {flag: element.flags,
+          currencies: Object.keys(element.currencies),
+          country: element.name}
+        })
+        // return {
+        //   flag: x.flags,
+        //   currencies:Object.keys( x.currencies),
+        //   country: x.name
+        // };
       })
-    ).subscribe(console.log)
+    );
   }
-
-  private fetchFlagsAndCurrencies(country:string) {
-    return this.movieApiService.getCountryDetails(country)
-    .pipe(
-      map((x:any) => x.map((x:any) => {
-        console.log(x)
-        return {
-          flag: x.flags,
-          currencies : x.currencies
-        }
-      }))
-      )
-  }
-
+  
 }
+
+
 
